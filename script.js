355dfee76a293be05c91466b793f8da7b0b79d72
Petros Demetriou <dimitriupe@gmail.com>
2022-08-13 11:33:59 +0300

    Initialized git and added files of project

---
+++ script.js
@@ -0,0 +1,46 @@
+// scrollFunction
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navigation").style.backgroundColor = "white";
+    document.getElementById("navbar").style.paddingBottom = "50px";
+    document.getElementById("navbar").style.transition = "0.8s";
+  } else {
+    document.getElementById("navigation").style.backgroundColor = "transparent";
+    document.getElementById("navbar").style.paddingBottom = "0px";
+    document.getElementById("navbar").style.transition = "0.8s";
+  }
+}
+
+// create instance of kinet with custom settings
+
+// var kinet = new Kinet({
+//     acceleration: 0.06,
+//     friction: 0.20,
+//     names: ["x", "y"],
+//   });
+  
+//   // select circle element
+//   var circle = document.getElementById('circle');
+  
+//   // set handler on kinet tick event
+//   kinet.on('tick', function(instances) {
+//     circle.style.transform = `translate3d(${ (instances.x.current) }px, ${ (instances.y.current) }px, 0) rotateX(${ (instances.x.velocity/2) }deg) rotateY(${ (instances.y.velocity/2) }deg)`;
+//   });
+  
+//   // call kinet animate method on mousemove
+//   document.addEventListener('mousemove', function (event) {
+//     kinet.animate('x', event.clientX - window.innerWidth/2);
+//     kinet.animate('y', event.clientY - window.innerHeight/2);
+//   });
+    
+//   // log
+//   kinet.on('start', function() {
+//     console.log('start');
+//   });
+  
+//   kinet.on('end', function() {
+//     console.log('end');
+//   });
