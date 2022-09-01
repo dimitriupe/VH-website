fd75ad2a08081967af4e199515e110fe23f063df
Petros Demetriou <dimitriupe@gmail.com>
2022-09-01 19:39:42 +0300

    Finalized modal function

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ const goToTop = () => {
 backToTopButton.addEventListener("click", goToTop)
 
 
-// slideShow function of Properties.html
+// slideShow function of pyrgos.html
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -87,6 +87,122 @@ function respondToPressedKey(e) {
 }
 
 document.body.addEventListener('keydown', respondToPressedKey);
+
+// OpenModal and CloseModal functions
+
+var modal = initModal();
+
+function initModal() {
+  return render({
+    tag: 'div',
+    id: 'myModal',
+    className: 'modal',
+    children: [
+      {
+        tag: 'span',
+        className: 'close cursor',
+        onclick: closeModal,
+        innerHTML: '&times;'
+      },
+      {
+        tag: 'div',
+        className: 'modal-content',
+        children: [
+          {
+            tag: 'div',
+            className: 'mySlidess',
+            children: [
+              {
+                tag: 'div',
+                className: 'numbertext'
+              },
+              {
+                tag: 'img',
+                style: 'width: 100%'
+              }
+            ]
+          },
+          {
+            tag: 'a',
+            className: 'previous',
+            onclick: function(){plusSlidess(-1)},
+            innerHTML: '&#10094'
+          },
+          {
+            tag: 'a',
+            className: 'nextt',
+            onclick: function(){plusSlidess(1)},
+            innerHTML: '&#10095'
+          }
+        ] 
+      }
+    ]
+  });
+}
+function render(obj) {
+  var el = document.createElement(obj.tag);
+  if(obj.children){
+    for(var i=0;i<obj.children.length;i++) {
+      el.appendChild(render(obj.children[i]));
+    }
+  }
+  delete el.tag;
+  delete el.children;
+  for(var key in obj){
+    el[key] = obj[key];
+  }
+  return el;
+}
+
+function openModal() {
+  document.body.appendChild(modal);
+}
+
+function closeModal() {
+  modal.remove();
+}
+
+var slideIndexes = 0;
+
+function plusSlidess(n) {
+  showSlidess(slideIndexes += n);
+}
+
+function currentSlide(n) {
+  showSlidess(slideIndexes = n);
+}
+
+function showSlidess(n) {
+  var i;
+  var slides = document.querySelectorAll('.column img');
+  // var captionText = modal.querySelector(".numbertext");
+  var modalImg = modal.querySelector('img');
+  n %= slides.length;
+  modalImg.src = slides[n].src;
+  // captionText.innerHTML = n + 1 + ' / ' + slides.length;
+}
+
+function respondToPresssedKey(e) {
+  switch (e.key) {
+    case 'ArrowLeft':
+      // run your ArrowLeft code here
+      plusSlidess(-1);
+      console.log('Arrow Left Key Pressed');
+      break;
+    case 'ArrowRight':
+      // run your ArrowRight code here
+      plusSlidess(+1);
+      console.log('Arrow Right Key Pressed');
+      break;
+    case 'Escape':
+      // run your Escape code here
+      closeModal();
+      console.log('Escape Key Pressed');
+      break;
+  }
+}
+
+document.body.addEventListener('keydown', respondToPresssedKey);
 
 // create instance of kinet with custom settings
 
